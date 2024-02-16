0c2fae8c61c79aec92d63c536cb9fc891b3e2a38
Войтенков Андрей <aavoitenko@mts.ru>
2024-02-17 01:02:48 +0300

    in process

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +13,14 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
+	var envDirMap Environment
+	dirEntries, err := os.ReadDir(dir)
+	for _, entry := range dirEntries {
+		if !entry.IsDir() {
+
+			// envDirMap[entry.Name()] =
+		}
+	}
+
 	return nil, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	commandWithArgs := os.Args[2:]
+	envDirMap, _ := ReadDir(os.Args[1])
+	for env, envValue := range envDirMap {
+		os.Unsetenv(env)
+		if envValue.NeedRemove {
+			delete(envDirMap, env)
+		} else {
+			os.Setenv(env, envValue.Value)
+		}
+	}
+
+	returnCode := RunCmd(commandWithArgs, envDirMap)
+	fmt.Println(returnCode)
 }
